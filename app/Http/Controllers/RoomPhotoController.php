22a7f424436b4882f2ad8f2208265d1280c619bb
arnas <arnassiusa@gmail.com>
2025-11-16 22:03:59 +0200

    chore: create controller stubs

---
+++ app/Http/Controllers/RoomPhotoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomPhoto;
+use Illuminate\Http\Request;
+
+class RoomPhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomPhoto $roomPhoto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomPhoto $roomPhoto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RoomPhoto $roomPhoto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomPhoto $roomPhoto)
+    {
+        //
+    }
+}
